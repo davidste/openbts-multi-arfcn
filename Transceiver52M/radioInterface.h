753c4a3b9157623be057f3160a6b095483f939bf
Thomas Tsou <ttsou@vt.edu>
2012-03-16 20:31:01 -0400

    multi-arfcn, trx: remove unused attach call()
    At one point an attach() call was used to connect
    multiple transceivers to the radio interface. The
    current approach is to pass the radio interface to
    the transceiver instances through the constructor.
    Remove the unused and deprecated call.
    
    Signed-off-by: Thomas Tsou <ttsou@vt.edu>

--- Transceiver52M/radioInterface.h
+++ Transceiver52M/radioInterface.h
@@ -90,6 +90,7 @@ public:
 
   /** start the interface */
   bool start();
+
   bool started() { return mOn; };
 
   /** shutdown interface */
@@ -112,9 +113,6 @@ public:
   /** check for underrun, resets underrun value */
   bool isUnderrun();
   
-  /** attach an existing USRP to this interface */
-  void attach(RadioDevice *wRadio, int wRadioOversampling);
-
   /** return the receive FIFO */
   VectorFIFO* receiveFIFO(int num) { return &mReceiveFIFO[num];}
 
